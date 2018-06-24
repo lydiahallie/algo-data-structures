ac9e2ebcf4c48cd75a3adacdc43359f2b581ea75
Lydia Hallie <lydiajuliettehallie@gmail.com>
2018-06-24 11:30:43 +0200

    Add bubble sort

---
+++ algorithms/bubble_sort.js
@@ -0,0 +1,26 @@
+const array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+
+function bubbleSort(array) {
+  let swapped;
+  do {
+    // Set var swapped to false as default value.
+    swapped = false;
+    // Loop over array.
+    for (let i = 0; i < array.length; i++) {
+      // If the array has the current value and the next value
+      // and the current value is bigger than the next value:
+      // the two values need to be swapped (assuming you want the items
+      // to be incrementally sorted).
+      if (array[i] && array[i + 1] && array[i] > array[i + 1]) {
+        // Set the current item to the next item, and the next item to the current item.
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        // Set swapped to true, meaning that the do-while loop will still run.
+        swapped = true;
+      }
+    }
+  } while(swapped);
+  // Return the array with the swapped values.
+  return array;
+}
+
+console.log(bubbleSort(array)); // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
